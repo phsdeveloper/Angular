4199760949656bb218fa489218994d370cf5e80a
Paulo Santos <phs.developer@gmail.com>
2021-02-23 17:38:22 -0300

    Item 07

---
+++ DevMedia/07-event-binding/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ClickMeComponent } from './click-me/click-me.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClickMeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
