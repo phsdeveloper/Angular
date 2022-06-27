9f114e2eae4ec9e6ba7720cf81ae4f2d5832613a
Paulo Santos | Casa <phs.developer@gmail.com>
2022-06-27 19:03:17 -0300

    Secao 04 - ✭ Event Binding: Aula finalizada com sucesso

--- Udemy/Secao04/data-binding/src/app/data-binding/data-binding.component.ts
+++ Udemy/Secao04/data-binding/src/app/data-binding/data-binding.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
@@ -7,12 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
-  public nome:string = "Paulo";
-  public idade:number = 40;
+  public nome: string = "Paulo";
+  public idade: number = 40;
+  public btn_title: string = "Button Property Binding";
+  public btn_Disabled: boolean = true;
+
+public position:{x:number,y:number} = {x:0,y:0};
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  chequedDisabled(): boolean {
+
+    return this.btn_Disabled;
+  }
+
+  alertInfo() {
+    alert("funcionou");
+    this.btn_Disabled = !this.btn_Disabled;
+    console.log(this.btn_Disabled);
+  }
+
+  alertInfoValor(valor: string) {
+    alert(valor);
+    this.btn_Disabled = !this.btn_Disabled;
+    console.log(this.btn_Disabled);
+  }
+
+  public alertInfoEvent(valor: MouseEvent) {
+    console.log(valor);
+
+  }
+
+  public MouseMoveTeste(valor: MouseEvent) {
+    console.log(valor);
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
+
 
 }
