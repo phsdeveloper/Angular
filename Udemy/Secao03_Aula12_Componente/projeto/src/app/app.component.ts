59be261190200407709c999ebdeced74700b7f15
Paulo Santos <paulo.phs82@hotmail.com>
2023-07-28 20:53:21 -0300

    ★ Secao 03 - Aula 14) Hack para organizar o app.component

--- Udemy/Secao03_Aula12_Componente/projeto/src/app/app.component.ts
+++ Udemy/Secao03_Aula12_Componente/projeto/src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
                                                         
 @Component({                                                                                            
   selector: 'app-root',                                                                                                   
-  templateUrl: './app.component.html',                                                        
-  styleUrls: ['./app.component.scss']                                                        
+  template:'<app-root></app-root>'                                                       
 })                                                        
 export class AppComponent {                                                        
   title = 'Projeto Seção 03';                                                        
