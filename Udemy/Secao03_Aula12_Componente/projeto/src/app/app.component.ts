e8da936ae7765360103e5a2a393c647d767ac8b3
Paulo Santos <paulo.phs82@hotmail.com>
2023-07-29 13:26:37 -0300

    ★ Secao 03 Aula 15) ngOnInit

--- Udemy/Secao03_Aula12_Componente/projeto/src/app/app.component.ts
+++ Udemy/Secao03_Aula12_Componente/projeto/src/app/app.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';  
+import { Component, OnInit } from '@angular/core';  
                                                         
 @Component({                                                                                            
   selector: 'app-root',                                                                                                   
-  template:'<app-root></app-root>'                                                       
+  template:'<router-outlet></router-outlet>'                                                       
 })                                                        
-export class AppComponent {                                                        
-  title = 'Projeto Seção 03';                                                        
+export class AppComponent implements OnInit{                                                        
+  title = 'Projeto Seção 03';  
+  public CiclosDeVida:string = "";
+  
+  
+  constructor(){
+    console.log("Construtor Chamado " + this.FUN_ObterDataAtual());
+
+    setTimeout(() => {
+      console.log("Construtor Chamado Timeout " + this.FUN_ObterDataAtual());
+    }, 3000);
+  }
+
+  ngOnInit(): void {
+    console.log("OnInit iniciado " + this.FUN_ObterDataAtual());
+      setTimeout(() => {
+        this.CiclosDeVida = "OnInit inicado após timeout";
+        console.log("OnInit inicado após timeout " + this.FUN_ObterDataAtual());
+      }, 5000);
+  }
+
+
+  FUN_ObterDataAtual(){
+    let DataAtual:Date = new Date;
+    let DataTela:String = DataAtual.getDate() + "/" + (DataAtual.getMonth() +1) + "/" + DataAtual.getFullYear() + " " + DataAtual.getHours() +":"+DataAtual.getMinutes()+ ":"+DataAtual.getSeconds();
+    
+    return DataTela;
+  }
 }                                                        
