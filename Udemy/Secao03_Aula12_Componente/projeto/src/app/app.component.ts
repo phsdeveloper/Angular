18b6ecf0983fccd2bf0235d6521e25ae1e4cd681
Paulo Santos <paulo.phs82@hotmail.com>
2023-07-31 07:57:20 -0300

    Secão 03 Aula 17 - Em andamento

--- Udemy/Secao03_Aula12_Componente/projeto/src/app/app.component.ts
+++ Udemy/Secao03_Aula12_Componente/projeto/src/app/app.component.ts
@@ -1,33 +1,107 @@
-import { Component, OnInit } from '@angular/core';  
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';  
                                                         
 @Component({                                                                                            
   selector: 'app-root',                                                                                                   
   template:`
             <div class="container">
-              <app-title ValorOnChanges= " Valor default alterado"></app-title>
-              <router-outlet></router-outlet>
+              <app-title></app-title>
+
+                <div div class="row col-sm-5 col-md-12 mt-4">
+                  <div class="card">
+                       <div class="card-header text-center"><h3><strong>{{title}}</strong></h3></div>
+                       <div class="card-body">
+                          <div class="row">
+                                    <div class="col-sm-5 col-md-6">
+                                      <div class="card">
+                                          <div class="card-header text-center">
+                                              <strong> Seção 03 Aula 117. ngDoCheck</strong>
+                                          </div>
+                                          <div class="card-body">
+                                              <h5 class="card-title">Dados verifição</h5>
+                                              <p class="card-text">{{Evento_Construtor}}</p>
+                                              <p class="card-text">{{Evento_ngAfterViewChecked}}</p>
+                                              <p class="card-text">{{Evento_ngAfterViewInit}}</p>
+                                              <p class="card-text">{{Evento_ngAfterContentChecked}}</p>
+                                              <p class="card-text">{{Evento_ngAfterContentInit}}</p>
+                                              <p class="card-text">{{Evento_ngDoCheck}}</p>
+                                              
+                                              <button class="btn btn-primary" (click)="FUN_Adicionar()">Adicionar</button>
+                                              <span> valor <strong> {{valor}}</strong> </span>
+                                          </div>
+                                      </div>
+                                    </div>
+                          </div>
+                       </div>
+                  </div>
+                </div>
+
+                <router-outlet></router-outlet>
             </div>
              
             `                                                     
 })                                                        
-export class AppComponent implements OnInit{                                                        
-  title = 'Projeto Seção 03';  
+export class AppComponent implements OnInit,
+                                     DoCheck,
+                                     AfterContentInit,
+                                     AfterContentChecked,
+                                     AfterViewInit,
+                                     AfterViewChecked 
+                                     
+
+{                                                        
+  title = 'Bem vindo ao app component';  
   public CiclosDeVida:string = "";
+  public Evento_ngAfterViewChecked:string = "";
+  public Evento_ngAfterViewInit:string = "";
+  public Evento_ngAfterContentChecked:string = "";
+  public Evento_ngAfterContentInit:string = "";
+  public Evento_ngDoCheck:string = "";
+
+  public Evento_Construtor:string="";
+
+  public valor:number =1;
+
   
-  
+
   constructor(){
     console.log("Construtor Chamado " + this.FUN_ObterDataAtual());
-
+    this.Evento_Construtor = "Construtor Chamado em: " + this.FUN_ObterDataAtual();
     setTimeout(() => {
-      console.log("Construtor Chamado Timeout " + this.FUN_ObterDataAtual());
+      //console.log("Construtor Chamado Timeout " + this.FUN_ObterDataAtual());
     }, 3000);
   }
 
+    ngDoCheck(): void {
+    this.Evento_ngDoCheck = "ngDoCheck             | Chamado em: " + this.FUN_ObterDataAtual();
+    console.log("ngDoCheck             | Chamado em: " + this.FUN_ObterDataAtual());
+  }
+
+    ngAfterContentInit(): void {
+    //this.Evento_ngAfterContentInit = "ngAfterContentInit Chamado em: " + this.FUN_ObterDataAtual();
+    console.log("ngAfterContentInit    | Chamado em: " + this.FUN_ObterDataAtual());
+  }
+
+
+  ngAfterViewChecked(): void {
+    //this.Evento_ngAfterViewChecked = "ngAfterViewChecked Chamado em: " + this.FUN_ObterDataAtual();
+     console.log("ngAfterViewChecked Chamado em: " + this.FUN_ObterDataAtual());
+  }
+  ngAfterViewInit(): void {
+    //this.Evento_ngAfterViewInit = "ngAfterViewInit Chamado em:" + this.FUN_ObterDataAtual();
+    console.log("ngAfterViewInit Chamado em: " + this.FUN_ObterDataAtual());
+  }
+  ngAfterContentChecked(): void {
+    //this.Evento_ngAfterContentChecked = "AfterContentChecked Chamado em: " + this.FUN_ObterDataAtual();
+    console.log("ngAfterContentChecked Chamado em: " + this.FUN_ObterDataAtual());
+  }
+
+
+
   ngOnInit(): void {
-    console.log("OnInit iniciado " + this.FUN_ObterDataAtual());
+    //console.log("OnInit iniciado " + this.FUN_ObterDataAtual());
       setTimeout(() => {
         this.CiclosDeVida = "OnInit inicado após timeout";
-        console.log("OnInit inicado após timeout " + this.FUN_ObterDataAtual());
+        //console.log("OnInit inicado após timeout " + this.FUN_ObterDataAtual());
       }, 5000);
   }
 
@@ -38,4 +112,39 @@ export class AppComponent implements OnInit{
     
     return DataTela;
   }
+  public FUN_Adicionar():number{
+    return this.valor+=1;
+  }
 }                                                        
+
+/**
+ * 
+ * 
+  Construtor Chamado 29/7/2023 17:39:12
+app.component.ts:91 ngDoCheck             | Chamado em: 29/7/2023 17:39:12
+app.component.ts:87 ngAfterContentInit    | Chamado em: 29/7/2023 17:39:12
+app.component.ts:83 ngAfterContentChecked | Chamado em: 29/7/2023 17:39:12
+app.component.ts:79 ngAfterViewInit       | Chamado em: 29/7/2023 17:39:12
+app.component.ts:75 ngAfterViewChecked    | Chamado em: 29/7/2023 17:39:12
+
+app.component.ts:91 ngDoCheck Chamado em: 29/7/2023 17:39:12
+app.component.ts:83 ngAfterContentChecked Chamado em: 29/7/2023 17:39:12
+app.component.ts:75 ngAfterViewChecked Chamado em: 29/7/2023 17:39:12
+app.component.ts:91 ngDoCheck Chamado em: 29/7/2023 17:39:12
+app.component.ts:83 ngAfterContentChecked Chamado em: 29/7/2023 17:39:12
+app.component.ts:75 ngAfterViewChecked Chamado em: 29/7/2023 17:39:12
+
+app.component.ts:91 ngDoCheck Chamado em: 29/7/2023 17:39:15
+app.component.ts:83 ngAfterContentChecked Chamado em: 29/7/2023 17:39:15
+app.component.ts:75 ngAfterViewChecked Chamado em: 29/7/2023 17:39:15
+
+app.component.ts:91 ngDoCheck Chamado em: 29/7/2023 17:39:17
+app.component.ts:83 ngAfterContentChecked Chamado em: 29/7/2023 17:39:17
+app.component.ts:75 ngAfterViewChecked Chamado em: 29/7/2023 17:39:17
+
+ngDoCheck Chamado em: 29/7/2023 17:40:44
+app.component.ts:83 ngAfterContentChecked Chamado em: 29/7/2023 17:40:44
+app.component.ts:75 ngAfterViewChecked Chamado em: 29/7/2023 17:40:44
+
+
+ */
