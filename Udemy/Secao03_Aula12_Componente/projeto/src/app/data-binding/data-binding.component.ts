f8372698a1678a0152ff99e241f1bb8f19bdbc09
Paulo Santos <paulo.phs82@hotmail.com>
2023-09-06 20:15:03 -0300

    Seção 04 Aula 22. Property Binding >> Finalizada

--- Udemy/Secao03_Aula12_Componente/projeto/src/app/data-binding/data-binding.component.ts
+++ Udemy/Secao03_Aula12_Componente/projeto/src/app/data-binding/data-binding.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
   styleUrls: ['./data-binding.component.scss']
 })
-export class DataBindingComponent {
+export class DataBindingComponent implements OnInit {
 
   public ComponentName:string = "data-binding";
   public title:string = "Bem vindo ao componente: " + this.ComponentName;
 
   public nome:string = "Paulo Santos";
+  public imgSRC:string = "https://angular.io/assets/images/logos/angular/logo-nav@2x.png";
+  public imgTitle:string = "Title via property Binding";
   public idade: number = 30;
 
+  public checkedDisabled:boolean = true;
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+      
+  }
 
 
 }
