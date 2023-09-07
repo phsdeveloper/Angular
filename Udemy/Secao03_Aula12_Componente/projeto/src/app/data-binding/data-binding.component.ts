ab76e1fee4dc80dd4f9b574d562fcc4234e36bbb
Paulo Santos <paulo.phs82@hotmail.com>
2023-09-06 23:11:49 -0300

    Seção 03 Aula 23. Event Binding >> Finalizada

--- Udemy/Secao03_Aula12_Componente/projeto/src/app/data-binding/data-binding.component.ts
+++ Udemy/Secao03_Aula12_Componente/projeto/src/app/data-binding/data-binding.component.ts
@@ -7,22 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
-  public ComponentName:string = "data-binding";
-  public title:string = "Bem vindo ao componente: " + this.ComponentName;
-
-  public nome:string = "Paulo Santos";
-  public imgSRC:string = "https://angular.io/assets/images/logos/angular/logo-nav@2x.png";
-  public imgTitle:string = "Title via property Binding";
+  public ComponentName: string = "data-binding";
+  public title: string = "Bem vindo ao componente: " + this.ComponentName;
+  // #region Aula 22
+  public nome: string = "Paulo Santos";
+  public imgSRC: string = "https://angular.io/assets/images/logos/angular/logo-nav@2x.png";
+  public imgTitle: string = "Title via property Binding";
   public idade: number = 30;
+  // #endregion Aula 22
 
-  public checkedDisabled:boolean = true;
 
-  constructor(){
+  public checkedDisabled: boolean = true;
+
+  constructor() {
 
   }
   ngOnInit(): void {
-      
+
   }
 
+  // #region Aula 23
+  public mensagemAlertTexto: string = "";
+  public mensagemAlertVisvel: boolean = false;
+
+  public position: { x: number, y: number, message: string } = { x: 0, y: 0, message: '' };
+
+  public FUN_alertaInfo(valor: MouseEvent): void {
+
+    this.mensagemAlertTexto = `Botão acionado em: ` + this.FUN_ObterDataAtual() + ' para mais informações veja o console';
+    console.log(valor);
+    this.mensagemAlertVisvel = true;
+  }
+
+  public FUN_MouseMovePosition(valor: MouseEvent): void {
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+    this.position.message = 'Valor alterado em: ' + this.FUN_ObterDataAtual();
+  }
+  // #endregion Aula 23
+
+  private FUN_ObterDataAtual() {
+    let DataAtual: Date = new Date;
+    let DataTela: String = DataAtual.getDate().toString().padStart(2, '0')
+      + "/" + (DataAtual.getMonth() + 1).toString().padStart(2, '0')
+      + "/" + DataAtual.getFullYear()
+      + " " + DataAtual.getHours().toString().padStart(2, '0')
+      + ":" + DataAtual.getMinutes().toString().padStart(2, '0')
+      + ":" + DataAtual.getSeconds().toString().padStart(2, '0')
+      + "." + DataAtual.getMilliseconds().toString().substring(0, 3);
+
+    return DataTela;
+  }
 
 }
